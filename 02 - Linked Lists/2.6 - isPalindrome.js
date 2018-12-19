78932e3bbc1432ef1204ec39d484cde2f8a71682
Chloe Lin <chloewjlin@gmail.com>
2018-12-19 01:37:14 -0800

    Fix bug in reverseAndCopyList and complete solution 1

--- 02 - Linked Lists/2.6 - isPalindrome.js
+++ 02 - Linked Lists/2.6 - isPalindrome.js
@@ -5,57 +5,64 @@ function ListNode(val) {
 };
 
 // Naive solution: push values to an array, then compare 
-// function isPalindrome(list) {
-//   if (!list) return false;
-//   let values = [];
-//   let p = list;
+function isPalindrome(list) {
+  if (!list) return false;
+  let values = [];
+  let p = list;
 
-//   while (p) {
-//     values.unshift(p.value);
-//     p = p.next;
-//   }
-
-//   return values.join('') === values.reverse().join('');
-// };
-
-// Solution 1: create a reversed list, them compare
-function reverseList(List) {
-  let node = List;
-  let previous; 
-  let temp;
-  
-  while (node) {
-    temp = node.next;
-    node.next = previous;
-    previous = node;
-    node = temp;
-  };
-
-  return previous;
-};
-
-function isEqual(ListOne, ListTwo) {
-  while (ListOne && ListTwo) {
-    if (ListOne.value !== ListTwo.value) {
-      return false;
-    }
-    ListOne = ListOne.next;
-    ListTwo = ListTwo.next;
+  while (p) {
+    values.unshift(p.value);
+    p = p.next;
   }
 
-  return ListOne == null && ListTwo == null;
+  return values.join('') === values.reverse().join('');
+};
+
+// Solution 1: create a reversed list, then compare
+
+// Note: this method is O(n) time complexity, O(n) space complexity
+function reverseAndCopyList(node) {
+  let previous = null;
+
+  while (node) {
+    var copy = new ListNode(node.value);
+    copy.next = previous;
+    previous = copy;
+    node = node.next;
+  };
+
+  return copy;
+};
+
+function isEqual(one, two) {
+  while (one !== null && two !== null) {
+    if (one.value !== two.value) {
+      return false;
+    } 
+    one = one.next;
+    two = two.next;
+  }
+
+  return one === null && two === null;
+};
+
+function isPalindrome(LinkedList) {
+  if (!LinkedList) return false;
+  let reversedList = reverseAndCopyList(LinkedList);
+  return isEqual(LinkedList, reversedList);
 };
 
 var oneNode = new ListNode(1);
+
 var list = new ListNode(1);
 list.next = new ListNode(2);
 list.next.next = new ListNode(3);
 list.next.next.next = new ListNode(2);
 list.next.next.next.next = new ListNode(1);
 
-// console.log(isPalindrome()); // false
-// console.log(isPalindrome(oneNode)); // true
-// console.log(isPalindrome(list)); // true
-// console.log(reverseList(list));
 console.log(isEqual(list, oneNode)); // false
 console.log(isEqual(list, list)); // true
+console.log(isPalindrome()); // false
+console.log(isPalindrome(oneNode)); // true
+console.log(isPalindrome(oneNode)); // true
+console.log(isPalindrome(list)); // true
