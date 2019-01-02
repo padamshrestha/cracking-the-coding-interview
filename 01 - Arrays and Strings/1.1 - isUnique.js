fe2e1eb5461219001b1d0020f126e30f1cde3cd9
Chloe Lin <chloewjlin@gmail.com>
2019-01-02 14:52:55 -0800

    Add O(nlogn) solution using sort

--- 01 - Arrays and Strings/1.1 - isUnique.js
+++ 01 - Arrays and Strings/1.1 - isUnique.js
@@ -12,12 +12,12 @@ const isUnique = function(string) {
     } else {
       count[string[i]]++;
     }
-  };
+  }
 
   for (key in count) {
     return count[key] > 1 ? false : true;
-  };
-}
+  }
+};
 
 // Solution 2: ES6 for-of loop
 // Time: O(n)
@@ -47,7 +47,23 @@ const isUnique = (str) => {
   return true;
 };
 
+// Solution 4: Sort characters
+// Time: O(nlogn)
+// Space: O(1)
+const isUnique = (str) => {
+  let strArr = [...str].sort();
+
+  for (let i = 0; i < strArr.length; i++) {
+    if (strArr[i] === strArr[i + 1]) return false;
+  }
+
+  return true;
+};
+
+// Test cases:
 console.log(isUnique('')); // true
 console.log(isUnique('abcde')); // true
 console.log(isUnique('aabb')); // false
-console.log(isUnique('@45327364asdjnaweioqwj+-=])?')); // false
+console.log(isUnique('gfedcba')); // true
+console.log(isUnique('02342935829031')); // false
+console.log(isUnique('@45327364asdjnaweioqwj+-=])?}')); // false
