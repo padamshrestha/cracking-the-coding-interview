0fc4837f6b9288275e8e8b331ba4fb194b82bb79
Chloe Lin <chloewjlin@gmail.com>
2019-01-02 16:38:24 -0800

    Complete O(nlogn) solution using sort

--- 01 - Arrays and Strings/1.2 - checkPermutations.js
+++ 01 - Arrays and Strings/1.2 - checkPermutations.js
@@ -1,6 +1,6 @@
 // Given two strings, write a method to decide if one is a permutation of the other.
 
-// Solution 1: hash table
+// Solution 1: hash table (two words with the same character counts)
 // Time: O(n)
 // Space: O(n)
 const checkPermutation = (str1, str2) => {
@@ -22,6 +22,20 @@ const checkPermutation = (str1, str2) => {
   return counter === 0;
 };
 
+// Solution 2: Sort the strings by characters (two words with the same characters in different orders)
+// Time: O(nlogn)
+// Space: O(n)
+const sortStr = (str) => {
+  return [...str].sort().join('');
+};
+
+const checkPermutation = (str1, str2) => {
+  if (!str1 || !str2 || str1.length !== str2.length) return false;
+  return sortStr(str1) === sortStr(str2);
+};
+
+// Test cases:
+// Note that our function is case sensitive and whitespace sensitive
 console.log(checkPermutation('', '')); // false
 console.log(checkPermutation('aaa', '')); // false
 console.log(checkPermutation('', 'aaa')); // false
