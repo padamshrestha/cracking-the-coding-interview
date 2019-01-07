e2791120e0bd795de92c2669c82f7ee8be9247a2
Chloe Lin <chloewjlin@gmail.com>
2019-01-06 21:14:48 -0800

    Complete implementing a graph class

--- 04 - Trees and Graphs/Graph.js
+++ 04 - Trees and Graphs/Graph.js
@@ -1,5 +1,6 @@
 // Implement a graph class.
 
+// Example:
 // Graph {
 //   nodes: {
 //      a: {b: true, c: true, d: true},
@@ -42,35 +43,47 @@ Graph.prototype.hasEdge = function(node, edge) {
   }
 };
 
-Graph.prototype.hasNode = function(edge) {
-
+Graph.prototype.hasNode = function(node) {
+  return this.nodes[node] !== undefined;
 };
 
-Graph.prototype.removeEdge = function(edge) {
-
+Graph.prototype.removeEdge = function(node, edge) {
+  if (this.nodes) {
+    delete this.nodes[node][edge];
+  }
 };
 
-Graph.prototype.removeNode = function(edge) {
-
+Graph.prototype.removeNode = function(node) {
+  if (this.nodes[node]) {
+    delete this.nodes[node];
+  }
 };
 
 // Tests
 
-let g = new Graph();
-g.addNode(0);
-g.addNode(1);
-g.addNode(2);
-g.addNode(3);
-g.addNode(4);
-g.addNode(5);
-g.addEdge(0, 1);
-g.addEdge(0, 4);
-g.addEdge(0, 5);
-g.addEdge(1, 3);
-g.addEdge(3, 2);
-g.addEdge(2, 1);
-g.addEdge(3, 4);
-console.log(g.findEdges(0)); // 1, 4, 5
-console.log(g.hasEdge(0, 6)); // false
-console.log(g.hasEdge(0, 1)); // true
-console.log(g);
+// let g = new Graph();
+// g.addNode(0);
+// g.addNode(1);
+// g.addNode(2);
+// g.addNode(3);
+// g.addNode(4);
+// g.addNode(5);
+// g.addEdge(0, 1);
+// g.addEdge(0, 4);
+// g.addEdge(0, 5);
+// g.addEdge(1, 3);
+// g.addEdge(3, 2);
+// g.addEdge(2, 1);
+// g.addEdge(3, 4);
+// console.log(g.findEdges(0)); // 1, 4, 5
+// console.log(g.hasEdge(0, 6)); // false
+// console.log(g.hasEdge(0, 1)); // true
+// console.log(g.hasNode(0)); // true
+// console.log(g.hasNode(7)); // false
+// g.removeEdge(3, 4);
+// console.log(g.hasEdge(3, 4)); // false
+// g.removeEdge(3, 6);
+// g.removeNode(5);
+// console.log(g.hasNode(5)); // false
+
+module.exports = Graph;
